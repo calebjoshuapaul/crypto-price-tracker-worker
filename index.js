da9958a03c5fb8eb66a93d0ab92e5cb20bd11e2d
Caleb Joshua <43238940+calebjoshuapaul@users.noreply.github.com>
2021-12-13 11:32:17 +0530

    update

--- index.js
+++ index.js
@@ -6,13 +6,11 @@ const API_ENDPOINT =
   "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest ";
 
 async function handleRequest(request) {
-  const data = fetch(API_ENDPOINT, {
+  const data = await fetch(API_ENDPOINT, {
     "X-CMC_PRO_API_KEY": `2243abfd-1d98-4e16-adb2-1e32fa1dab1d`,
   })
     .then((response) => response.json())
     .then((data) => data);
-
-  console.log(JSON.stringify(data));
 
   return new Response(JSON.stringify(data), {
     headers: {
